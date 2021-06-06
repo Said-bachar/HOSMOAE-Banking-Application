c240cab39f6d0aa9a42037730693e98774c8b1c6
Said <saidbachar.uca.ma@gmail.com>
2021-06-06 11:49:15 +0100

    [dev] Said : Adding The Transfer Repo

--- src/main/java/com/ensa/hosmoaBank/repositories/TransferRepository.java
+++ src/main/java/com/ensa/hosmoaBank/repositories/TransferRepository.java
@@ -1,13 +1,17 @@
 package com.ensa.hosmoaBank.repositories;
 
-import java.util.Collection;
+
+import java.util.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 import com.ensa.hosmoaBank.models.Client;
 import com.ensa.hosmoaBank.models.Transfer;
 
+@RepositoryRestController
 public interface TransferRepository extends JpaRepository<Transfer, Long>{
 	
-	public Collection<Transfer> findAllByClient(Client client);
+	Optional<Transfer> findByIdAndAndCompte_Client(Long id, Client client);
+	List<Transfer> findAllByCompte_Client(Client client);
 }
