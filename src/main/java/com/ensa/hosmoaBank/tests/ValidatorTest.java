2b8efe1052290374d3c0f044c122656f5bffdd5e
Said <saidbachar.uca.ma@gmail.com>
2021-06-06 22:56:56 +0100

    [dev] Said : Some tests

---
+++ src/main/java/com/ensa/hosmoaBank/tests/ValidatorTest.java
@@ -0,0 +1,30 @@
+package com.ensa.hosmoaBank.tests;
+
+
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//
+//import com.ensa.hosmoaBank.models.AccountCredentialsRequest;
+//
+//import javax.validation.ConstraintViolation;
+//import javax.validation.Validator;
+//import java.util.Set;
+//
+//
+//
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+public class ValidatorTest {
+
+//    @Autowired
+//    private Validator validator;
+//
+//    @Test
+//    public void shouldGiveInvalidNumber () {
+//        AccountCredentialsRequest c = new AccountCredentialsRequest("2442365958750258", "asdfghjk");
+//        Set<ConstraintViolation<AccountCredentialsRequest>> violationSet = validator.validate(c);
+//        assertEquals(0, violationSet.size());
+//    }
+
+}
