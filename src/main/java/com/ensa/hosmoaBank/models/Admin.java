c646325e43298db16020b4f6223384eb0860e078
Said <saidbachar.uca.ma@gmail.com>
2021-06-06 02:27:04 +0100

    [dev] Said : Updating the Admin Model -> Adding some new attributs

--- src/main/java/com/ensa/hosmoaBank/models/Admin.java
+++ src/main/java/com/ensa/hosmoaBank/models/Admin.java
@@ -3,6 +3,11 @@ package com.ensa.hosmoaBank.models;
 import java.util.*;
 
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
 import lombok.*;
 
  
@@ -10,16 +15,32 @@ import lombok.*;
 @Entity
 @Table(name = "admins")
 @Getter
-public class Admin extends User{
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonPropertyOrder({ "user" })
+public class Admin {
 	
-	  //Relation: 1 Admin ----> * Agencies
-	  
-	  @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE}) 
-	  Collection<Agency> agencies; 
-	  //Relation: 1 Admin ---->  * Agents
-	  
-	  @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE}) 
-	  Collection<Agent> agents;
+	@Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY,  cascade={CascadeType.REMOVE}) // pour la relation : chaque admin a pls agents
+    Collection<Agent> agents;
+
+    @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY,  cascade={CascadeType.REMOVE})// pour la relation : chaque admin a pls agences
+    Collection<Agency> agences;
+
+    @OneToOne // pour la relation : un admin a un compte user pour la auth
+    @JoinColumn(name = "id_user")
+    @JsonIgnoreProperties({"admin", "emailConfirmed", "dateUpdate", "dateOfCreation", "id" })
+    @JsonUnwrapped
+    private User user;
+
+    //Just for test
+    public Admin(User user) {
+        this.user=user;
+    }
 	 
       
 }
