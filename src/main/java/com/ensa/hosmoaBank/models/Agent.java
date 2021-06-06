e654490c9f3b0d9e7174dfab302f3076fa957c10
Said <saidbachar.uca.ma@gmail.com>
2021-06-06 02:52:15 +0100

    [dev] Said : * Updating the Agent Model

--- src/main/java/com/ensa/hosmoaBank/models/Agent.java
+++ src/main/java/com/ensa/hosmoaBank/models/Agent.java
@@ -4,15 +4,60 @@ import java.util.Collection;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
+import lombok.*;
+
 @Entity
-public class Agent extends User{
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@JsonPropertyOrder({ "user" })
+public class Agent {
     
-	@ManyToOne @JoinColumn(name = "id_admin", nullable = false)
-	private Admin admin;
-	@ManyToOne @JoinColumn(name = "id_agency")
-	private Agency agency;
-	@OneToMany(mappedBy = "agent", fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE}) 
-	Collection<Client> clients;
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_admin") // Relation : every Agent ------< 1 Admin
+	//@NotNull
+	//@JsonIgnore
+    @JsonIgnoreProperties({"agents", "agencies"})
+    private Admin admin;
+
+    @ManyToOne
+    @JoinColumn(name = "id_agency") // Relation : 1 Agent ---->  1 Agency
+	//@NotNull
+    @JsonIgnoreProperties({"admin", "agents", "clients"})
+	//@JsonIgnore
+    private Agency agency;
+
+    @OneToMany(mappedBy = "agent", fetch = FetchType.LAZY,  cascade={CascadeType.REMOVE})// Relation : every Agent ---->0..* clients
+    @JsonIgnoreProperties({"agent","agency"})
+	//@JsonIgnore
+    private Collection<Client> clients;
+
+    @OneToOne // pour la relation : un admin a un compte user pour la auth
+    @JoinColumn(name = "id_user")
+	//@JsonIgnore
+    @JsonIgnoreProperties({"agent","id"})
+    @JsonUnwrapped
+    private User user;
+
+    //Just for test
+
+    public Agent(User user ,Admin admin , Agency agency) {
+        this.user=user;
+        this.admin=admin;
+        this.agency=agency;
+    }
+
 	
 	
 }
