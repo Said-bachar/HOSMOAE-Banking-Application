20db86721a3580d4f68815c61112c7aef9a74580
Said <saidbachar.uca.ma@gmail.com>
2021-06-06 13:25:11 +0100

    [dev] Said : Update the Auth service

--- src/main/java/com/ensa/hosmoaBank/services/AuthService.java
+++ src/main/java/com/ensa/hosmoaBank/services/AuthService.java
@@ -1,18 +1,20 @@
 package com.ensa.hosmoaBank.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.ensa.hosmoaBank.enumerations.Role;
 import com.ensa.hosmoaBank.models.User;
-import com.ensa.hosmoaBank.repositories.AgentRepository;
 import com.ensa.hosmoaBank.repositories.UserRepository;
 import com.ensa.hosmoaBank.utilities.JWTUtils;
 
@@ -23,63 +25,56 @@ import lombok.Data;
 public class AuthService implements UserDetailsService { //implements UserDetailsService
 	
 	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private AgentRepository agentRepository;
-	
-	@Autowired
-	private JWTUtils jwtUtils;
-	
-	
-	
-	@Autowired 
-	private AuthenticationManager authenticationManager;
-	 
+    private UserRepository userRepository;
 
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(email);
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user.getAuthorities());
-	}
-	
-	
-	// Auth :
-	
-	
-	  public void authenticate(String email, String password) { 
-		  try {
-			  this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));    
-		  } catch(DisabledException e) { 
-			  e.printStackTrace(); // try to personnalise this
-		  } catch(BadCredentialsException e) {
-		        e.printStackTrace(); // too }
-	      }
-	  }
-	 
-	public User getCurrentUser() {   ///!!\ getCurrent{ADMIN | AGENT | CLIENT}
-		
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if(principal instanceof UserDetails) {
-			return this.userRepository.findByEmail(((UserDetails) principal).getPassword());
-		} else {
-			return null;
-		}
-		
-		//return jwtUtils.getUserFromToken();
-	}
-	
-//	public User getCurrentAgent() {   ///!!\ getCurrent{ADMIN | AGENT | CLIENT}
-//		
-//		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//		if(principal instanceof UserDetails) {
-//			return this.agentRepository.findByEmail(((UserDetails) principal).getPassword());
-//		} else {
-//			return null;
-//		}
-//		
-//		//return jwtUtils.getUserFromToken();
-//	}
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user.getAuthorities());
+    }
+
+    public void authenticate(String email, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+        }
+    public void agentAuthenticate(String email, String password , Role role) throws Exception {
+        System.out.println(role);
+        if (role.equals(Role.AGENT)) {
+            try {
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+            } catch (DisabledException e) {
+                throw new Exception("USER_DISABLED", e);
+            } catch (BadCredentialsException e) {
+                throw new Exception("INVALID_CREDENTIALS", e);
+            }
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    public User getCurrentUser() {
+        return jwtUtils.getUserFromToken();
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        if (principal instanceof UserDetails) {
+//            return userRepository.findByEmail(((UserDetails) principal).getUsername());
+//        } else {
+//            return null;
+//        }
+    }
 	
 	
 
